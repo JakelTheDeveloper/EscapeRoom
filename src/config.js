48f145e457c81adf3779cf979b618ffd2cb88f87
JaKel Jenkins <jakelj24@gmail.com>
2021-01-24 16:26:16 -0500

    update

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
     API_KEY: process.env.REACT_APP_API_KEY,
-    // URL: "https://desolate-dusk-00206.herokuapp.com"
-    URL:"http://localhost:8000"
+    URL: "https://desolate-dusk-00206.herokuapp.com"
+    // URL:"http://localhost:8000"
 }
